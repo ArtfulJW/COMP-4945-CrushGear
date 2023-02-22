ebd21f2127c7b6bff0a144fa8b49fa8ab1d0c0be
Tissure <kyle.ng7512@gmail.com>
2023-02-22 11:18:30 -0800

    Implement basic race triggers - TODO make triggers prefabs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,76 @@ using Unity.Netcode;
 
 public class Player : NetworkBehaviour
 {
+    [SerializeField]
+    private TrackInfo trackInfo;
+    [SerializeField]
+    private int triggerAmount;
+    [SerializeField]
+    private int triggerCounter = 0;
+    [SerializeField]
+    private int lapCounter = 0;
 
     [SerializeField]
+    private NetworkVariable<int> networkTriggerCounter = new NetworkVariable<int>();
 
+    [SerializeField]
+    private NetworkVariable<int> networkLapCounter = new NetworkVariable<int>();
+
+    private void Start()
+    {
+        // Init track info
+        trackInfo = GameObject.Find("Track").GetComponent<TrackInfo>();
+        triggerAmount = trackInfo.triggers.Count;
+    }
+
+    /// <summary>
+    /// Proccess Trigger events
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter Trigger");
+        if (IsClient && IsOwner)
+        {
+            Debug.Log("Enter Trigger");
+            if (other.tag == "PathPointTrigger")
+            {
+                other.gameObject.SetActive(false);
+                triggerCounter++;
+            }
+            else if (other.tag == "Goal")
+            {
+                other.gameObject.SetActive(false);
+                lapCounter++;
+                triggerCounter = 0;
+            }
+            UpdateClientServerRpc(triggerCounter, lapCounter);
+            processTrigger();
+        }
+        
+    }
+
+    [ServerRpc]
+    public void UpdateClientServerRpc(int triggerCount, int lapCount)
+    {
+        networkTriggerCounter.Value = triggerCount;
+        networkLapCounter.Value = lapCount;
+    }
+
+    /// <summary>
+    /// Activates next trigger point in lap.
+    /// </summary>
+    void processTrigger()
+    {
+        Debug.Log("Processing Triggers");
+        if (networkTriggerCounter.Value < triggerAmount)
+        {
+            trackInfo.triggers[networkTriggerCounter.Value].gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Activate Goal");
+            trackInfo.goal.transform.Find("GoalTrigger").gameObject.SetActive(true);
+        }
+            
     }
 }
